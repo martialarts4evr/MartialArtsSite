dd15901d6ecdc37f4725ba5b2ed00dc06c62dce3
meira <meira.datiya@gmail.com>
2015-12-20 14:32:41 -0600

    404, Title, Bio updated, Text
    Cleaned up 404 page to match index.html, renamed title,

---
+++ texttoadd.cs
@@ -0,0 +1,15 @@
+Danny Hoskins                                                            Started Martial Arts training at the age of 5 with Grandfather King Chamblin who was a WW2 Vet and retired Detective.
+I have Teacher/Black Belt/Guro Ranks in
+-Serrada Esgrima
+Modern Arnis Karate JuJitsu
+-Shorinji Ryu JuJitsu
+-American Carny Style Catch as Catch Can Wrestling
+Esgrima Criolla
+-Antrim Style Irish Stick Fighting
+Master Ranks in
+-Combat Glima
+-Chulukua
+
+Been researching Native American Warrior Arts for 30 years.
+
+Warriors Path is an ongoing Research-Study-Practice of Indigenous, Tribal and Cultural Warrior Fighting arts found through out the America's and Europe.
